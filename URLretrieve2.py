07fe1cf7422c474330496d524be9a452c8ed710f
wsheehan19 <78872401+wsheehan19@users.noreply.github.com>
2021-03-07 20:49:41 -0500

    Add files via upload

--- URLretrieve2.py
+++ URLretrieve2.py
@@ -21,9 +21,7 @@ from bs4 import BeautifulSoup as bs
 
 
 PATH = "/Users/williamsheehan/Downloads/chromedriver 3"
-driver = webdriver.Chrome(PATH)
 
-url = """https://www.equibase.com/premium/eqbPDFChartPlus.cfm?RACE=12&BorP=P&TID=GP&CTRY=USA&DT=01/23/2021&DAY=D&STYLE=EQB/"""
 
 # def check(url):
 #     req = requests.head(url)
@@ -35,23 +33,16 @@ url = """https://www.equibase.com/premium/eqbPDFChartPlus.cfm?RACE=12&BorP=P&TID
 #     else:
 #         return False
 
-def check(url):
-    UAS = ("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1",
-            "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0",
-            "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
-            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
-            )
-    ua = UAS[random.randrange(len(UAS))]
-    headers = {'user-agent': ua}    
-    r = requests.get(url, headers = headers)
-    content = str(r.content)
-    if 'Request unsuccessful' in content:
-        return False
-    else:
-        return True
-    
+# def check(url):
+#         content = str(r.content)
+#         print(content)
+#         if 'Request unsuccessful' in content:
+#             return False
+#         else:
+#             return True
+#             print('good')
+#         if counter == len(UAS):
+#             print('bad')
     
 def download(url):
     UAS = ("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1",
@@ -64,28 +55,35 @@ def download(url):
     ua = UAS[random.randrange(len(UAS))]
     headers = {'user-agent': ua}
     r=requests.get(url, headers=headers, stream = True)
+    text = r.text
+    if 'Helvetica' in text:
+        filename = url.split("/")[4].split(".cfm")[0]
     
-    filename = url.split("/")[4].split(".cfm")[0]
-    
-    file = open(filename, 'wb')
-    file.write(r.content)
-    file.close()
-
+        file = open(filename, 'wb')
+        file.write(r.content)
+        file.close()
+    else:
+        print('shme')
 
 with open('random300.json', 'r') as f:
      urls = json.load(f)
 
-for url in urls:
-    if check(url):
-        download(url)
-        print('file downloading')
-    else:
-        pass
-        print('blank page')
+# for url in urls:
+#     if check(url):
+#         download(url)
+#         print('file downloading')
+#     else:
+#         pass
+#         print('blank page')
 
-
+#print(urls)
 
 # soup = bs(r.text, 'html.parser')
 # print(soup)
 
+good_url = "https://www.equibase.com/premium/eqbPDFChartPlus.cfm?RACE=12&BorP=P&TID=GP&CTRY=USA&DT=01/23/2021&DAY=D&STYLE=EQB/"
+bad_url = "https://www.equibase.com/premium/eqbPDFChartPlus.cfm?RACE=1&BorP=P&TID=BTP&CTRY=USA&DT=10/06/2005&DAY=D&STYLE=EQB"
+
+
+download(good_url)
 

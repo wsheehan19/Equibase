c4a358198bf8df899fed981930dbc00821891fd6
wsheehan19 <78872401+wsheehan19@users.noreply.github.com>
2021-03-10 15:31:24 -0500

    Working Code, but..
    I also attached the text file that is generated by calling that function. This is an embarrassing question, but I cannot figure out where the PDF file to read is even being passed in the function. It only reads in one of the PDFs from that bucket, but I never specified which one.

--- readpdfs1.py
+++ readpdfs1.py
@@ -10,6 +10,7 @@ import re
 from google.cloud import vision
 from google.cloud import storage
 import os
+
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/williamsheehan/Downloads/Equibase-4f0a3e50cac6.json"
 
 
@@ -21,7 +22,9 @@ def read(destination):
     bucket_name = match.group(1)
     prefix = match.group(2)
     bucket = storage_client.get_bucket(bucket_name)
+    print(bucket)
     blob_list = list(bucket.list_blobs(prefix=prefix))
+    print(blob_list)
     print('Output files:')
     for blob in blob_list:
         print(blob.name)
@@ -29,12 +32,12 @@ def read(destination):
          output = blob_list[n]
          json_string = output.download_as_string()
     response = json.loads(json_string.decode())
-    file = open("batch{}.txt".format(str(n)), "wb")
+    file = open("batch{}.txt".format(str(n)), "w+")
     for m in range(len(response['responses'])):
-        first_page_response = response['response'][m]
+        first_page_response = response['responses'][m]
         annotation = first_page_response['fullTextAnnotation']
-        print('Full text:\n')
-        print(annotation['text'])
+        #print('Full text:\n')
+        #print(annotation['text'])
         file.write(annotation['text'])     
     
     

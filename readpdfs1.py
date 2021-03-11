e8abdeb4524ea2b17130055f7f7624fae1e33594
wsheehan19 <78872401+wsheehan19@users.noreply.github.com>
2021-03-11 14:21:56 -0500

    Seems to be working but text files not showing up

--- readpdfs1.py
+++ readpdfs1.py
@@ -10,11 +10,30 @@ import re
 from google.cloud import vision
 from google.cloud import storage
 import os
+from json import JSONDecodeError
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/williamsheehan/Downloads/Equibase-4f0a3e50cac6.json"
 
+def async_detect_document(gcs_source_uri, gcs_destination_uri):
+    mime_type = 'application/pdf'
+    batch_size = 100
+    client = vision.ImageAnnotatorClient()
+    feature = vision.Feature(
+        type_ = vision.Feature.Type.DOCUMENT_TEXT_DETECTION) ###### FLAG
+    gcs_source = vision.GcsSource(uri = gcs_source_uri) ### FLAG
+    input_config = vision.InputConfig(
+        gcs_source = gcs_source, mime_type = mime_type)
+    gcs_destination = vision.GcsDestination(uri=gcs_destination_uri)
+    output_config = vision.OutputConfig(
+        gcs_destination=gcs_destination, batch_size=batch_size)
+    async_request = vision.AsyncAnnotateFileRequest(
+        features=[feature], input_config=input_config,
+        output_config=output_config)
+    operation = client.async_batch_annotate_files(
+        requests=[async_request])
+    print('Waiting for operation to finish')
+    operation.result(timeout=400)
 
-gcs_destination_uri = 'gs://equibasestorage/Equibase Charts/output URI'
 
 def read(destination):
     storage_client = storage.Client()
@@ -22,17 +41,18 @@ def read(destination):
     bucket_name = match.group(1)
     prefix = match.group(2)
     bucket = storage_client.get_bucket(bucket_name)
-    print(bucket)
     blob_list = list(bucket.list_blobs(prefix=prefix))
-    print(blob_list)
     print('Output files:')
     for blob in blob_list:
         print(blob.name)
     for n in range(len(blob_list)):
          output = blob_list[n]
          json_string = output.download_as_string()
-    response = json.loads(json_string.decode())
-    file = open("batch{}.txt".format(str(n)), "w+")
+         try:
+             response = json.loads(json_string.decode())
+         except JSONDecodeError:
+             pass
+         file = open("batch{}.txt".format(str(n)), "w+")
     for m in range(len(response['responses'])):
         first_page_response = response['responses'][m]
         annotation = first_page_response['fullTextAnnotation']
@@ -40,6 +60,13 @@ def read(destination):
         #print(annotation['text'])
         file.write(annotation['text'])     
     
-    
+    return None
 
-read(gcs_destination_uri)
+
+gcs_destination_uri = 'gs://equibasestorage/Equibase Charts/output URI'
+for n in range(0,4):
+    if n != 0:
+        gcs_source_uri = 'gs://equibasestorage/Equibase Charts/' + 'eqbPDFChartPlus ({}).pdf'.format(n)
+        async_detect_document(gcs_source_uri, gcs_destination_uri)
+        read(gcs_destination_uri)
+

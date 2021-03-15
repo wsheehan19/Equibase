8baa3f03d13e598f7cb2348e62486ab4154191a7
wsheehan19 <78872401+wsheehan19@users.noreply.github.com>
2021-03-15 11:55:56 -0400

    Progress from downloading pdfs to writing content to text file

---
+++ FullScope.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 15 11:24:15 2021
+
+@author: williamsheehan
+"""
+import json
+import os
+import requests
+import random
+import time
+from google.cloud import storage
+import re
+from google.cloud import vision
+from json import JSONDecodeError
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/williamsheehan/Downloads/Equibase-4f0a3e50cac6.json"
+    
+def download(url):
+    """
+    downloads a pdf to local folder
+
+    Parameters
+    ----------
+    url : PDF url stored in json file
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    
+    UAS = ("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1",
+            "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0",
+            "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+            )
+    ua = UAS[random.randrange(len(UAS))]
+    headers = {'user-agent': ua}
+    r=requests.get(url, headers=headers, stream = True)
+    text = r.text
+    if 'Helvetica' in text:
+#        filename = url.split("/")[4].split(".cfm")[0]
+        filename = url.split('?')[1].split('/')[0]
+        file = open(filename, 'wb+')
+        file.write(r.content)
+        file.close()
+        print('download')
+        return None
+    if "ROBOTS" in text:
+        print('text')
+        return text
+    else:
+        print('invalid')
+        return None
+    
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """
+    uploads local pdf file to Google Cloud Storage bucket
+    
+    Parameters
+    ----------
+    bucket_name : bucket name
+    
+    source_file_name : local path to file
+    
+    destination_blob_name : storage object name
+
+    Returns
+    -------
+    None.
+
+    """
+
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print("File {} uploaded to {}.".format(source_file_name, destination_blob_name))
+    
+    return None
+
+
+def async_detect_document(gcs_source_uri, gcs_destination_uri):
+    """
+    Annotates PDF document with text detection and saves .txt file to local folder
+
+    Parameters
+    ----------
+    gcs_source_uri : gcs path to PDF image
+    
+    gcs_destination_uri : gcs json file which will be written to .txt file
+
+    Returns
+    -------
+    None.
+
+    """
+    client = vision.ImageAnnotatorClient()
+
+    batch_size = 10
+    mime_type = 'application/pdf'
+    feature = vision.Feature(
+        type_=vision.Feature.Type.DOCUMENT_TEXT_DETECTION)
+    
+    gcs_source = vision.GcsSource(uri = gcs_source_uri)
+    input_config = vision.InputConfig(gcs_source=gcs_source, mime_type=mime_type)
+    
+    gcs_destination = vision.GcsDestination(uri=gcs_destination_uri)
+    output_config = vision.OutputConfig(gcs_destination=gcs_destination, batch_size = batch_size)
+    async_request = vision.AsyncAnnotateFileRequest(
+        features=[feature], input_config=input_config, output_config=output_config)
+    
+    operation = client.async_batch_annotate_files(requests=[async_request])
+    operation.result(timeout=180)
+    
+    storage_client = storage.Client()
+    match = re.match(r'gs://([^/]+)/(.+)', gcs_destination_uri)
+    bucket_name = match.group(1)
+    prefix = match.group(2)
+    bucket = storage_client.get_bucket(bucket_name)
+    
+    blob_list = list(bucket.list_blobs(prefix=prefix))
+    
+    for n in range(1, len(blob_list)):
+        output = blob_list[n]
+        json_string = output.download_as_string()
+        try:
+            response = json.loads(json_string)
+    
+            first_page_response = response['responses'][0]
+            annotation = first_page_response['fullTextAnnotation']
+    
+            #print('Full text:\n')
+            file = open('chart{}.txt'.format(n), 'w+')
+            #print(annotation['text'])
+            file.write(annotation['text'])
+        except JSONDecodeError:
+            print('jsondecode')
+            pass
+    return None
+
+
+
+error_arr = []        
+with open ('fullpdfurls.json', 'r') as f:
+    urls = json.load(f)    
+
+for url in urls:
+    a = download(url)
+    time.sleep(90)
+    if a is not None:
+        error_arr += [a]
+        
+    else:
+        file = url.split('?')[1].split('/')[0]
+        path = "/Users/williamsheehan/Documents/Equibase.pys/all_images/" + file
+        upload_blob('equibasestorage', path,'Equibase Charts/' )
+        
+        gcs_path = 'gs://equibasestorage/Equibase Charts/' + file
+        gcs_destination_uri = 'gs://equibasestorage/Equibase Charts/output URI'
+        
+        async_detect_document(gcs_path, gcs_destination_uri)
+
+
+with open('errorURLS.json', 'w+') as fp:
+    fp.write(error_arr)
+            
+
+
+
+
+
+
+
+
+
+
+
+    
+    
